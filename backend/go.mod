b40eb1cb8cfd9e1cc00f5c2e8da19a6dde7ef74a
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2022-06-07 14:14:07 +0300

    feat: backend add dockerfile and update docker-compose

--- backend/go.mod
+++ backend/go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/cors v1.8.2 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
--- backend/router/server.go
+++ backend/router/server.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func StartServer() {
@@ -19,9 +20,17 @@ func StartServer() {
 	r.HandleFunc("/auth/register", controllers.Register).Methods("POST")
 	r.HandleFunc("/auth/login", controllers.Login).Methods("POST")
 
+	// CORS
+	corsObj := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE"},
+	})
+
 	// Start server
 	srv := &http.Server{
-		Handler:      r,
+		Handler:      corsObj.Handler(r),
 		Addr:         "127.0.0.1:3001",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
